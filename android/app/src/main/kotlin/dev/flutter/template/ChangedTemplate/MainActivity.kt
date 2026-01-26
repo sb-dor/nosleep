552ba0a2687e2fd1f4fc976d472eed3d701fb08a
sb-dor <avaz.shams.2002@gmail.com>
2026-01-26 21:57:58 +0500

    initialization

---
+++ android/app/src/main/kotlin/dev/flutter/template/ChangedTemplate/MainActivity.kt
@@ -0,0 +1,5 @@
+package www.sbdor.template.no_sleep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/feature/authentication/data/authentication_repository.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:no_sleep/src/common/util/api_client.dart';
+import 'package:no_sleep/src/feature/authentication/model/sign_in_data.dart';
+import 'package:no_sleep/src/feature/authentication/model/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract interface class IAuthenticationRepository {
+  Future<User> signIn(SignInData data);
+}
+
+class AuthenticationRepositoryImpl implements IAuthenticationRepository {
+  AuthenticationRepositoryImpl({
+    required final SharedPreferences sharedPreferences,
+    required final ApiClient apiClient,
+  }) : _sharedPreferences = sharedPreferences,
+       _apiClient = apiClient;
+
+  final SharedPreferences _sharedPreferences;
+  final ApiClient _apiClient;
+
+  @override
+  Future<User> signIn(SignInData data) {
+    throw UnimplementedError();
+  }
+}
+
+class AuthenticationRepositoryFake implements IAuthenticationRepository {
+  @override
+  Future<User> signIn(SignInData data) async => User.defaultUser();
+}
